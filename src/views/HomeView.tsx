add0bcf11beac4769764d06a04fade074716cbb1
tesarlukas <lukastesard90@gmail.com>
2023-06-17 17:16:17 +0200

    Test Firestore connection

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,17 +1,29 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { VStack } from 'native-base'
+import { Button, VStack } from 'native-base'
 import { StackParamsList } from '../components/navigation/types'
+import { collection, addDoc } from 'firebase/firestore'
+import { FIRESTORE_DB } from '../../firebaseConfig'
 
 export type HomeProps = NativeStackScreenProps<StackParamsList, 'Home'>
 
 const HomeView = () => {
+  const handleAddDoc = async () => {
+    try {
+      const docRef = await addDoc(collection(FIRESTORE_DB, 'users'), {
+        first: 'Ada',
+        last: 'Lovelace',
+        born: 1815,
+      })
+      console.log('Document written with ID: ', docRef.id)
+    } catch (e) {
+      console.error('Error adding document: ', e)
+    }
+  }
+
   return (
-    <VStack
-      flex={1}
-      bg='#fff'
-      alignItems='center'
-      justifyContent='flex-start'
-    ></VStack>
+    <VStack flex={1} bg='#fff' alignItems='center' justifyContent='flex-start'>
+      <Button onPress={handleAddDoc}>Press</Button>
+    </VStack>
   )
 }
 
