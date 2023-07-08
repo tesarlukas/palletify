06bff6675271da4583797f97cb624072cff45a6d
tesarlukas <lukastesard90@gmail.com>
2023-07-08 16:25:06 +0200

    Add toggle between sign in and sign up

--- src/views/AuthView.tsx
+++ src/views/AuthView.tsx
@@ -1,11 +1,35 @@
 import { Center, Text, View } from 'native-base'
+import { useState } from 'react'
 import { Login, Register } from '../components/auth'
 
 export const AuthView = () => {
+  const [isRegistered, setIsRegistered] = useState<boolean>(false)
+
   return (
     <View flex={1}>
-      <Register />
-      <Login />
+      <Center>
+        {isRegistered ? (
+          <>
+            <Login />
+            <Text>
+              Not yet registered?{' '}
+              <Text color='primary.500' onPress={() => setIsRegistered(false)}>
+                Sign up!
+              </Text>
+            </Text>
+          </>
+        ) : (
+          <>
+            <Register />
+            <Text>
+              Already registered?{' '}
+              <Text color='primary.500' onPress={() => setIsRegistered(true)}>
+                Sign in!
+              </Text>
+            </Text>
+          </>
+        )}
+      </Center>
     </View>
   )
 }
